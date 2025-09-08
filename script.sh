b090398a7786e3cd986b62348f4294a5b0a01391
jorgeloopzz <jorgeloopzz@protonmail.com>
2025-09-08 10:40:23 +0100

    Enable detection of bootable partitions and kill program on either cases

--- script.sh
+++ script.sh
@@ -4,6 +4,7 @@
 hard_drive="/dev/$1"
 mountpoint=`lsblk | grep $1 | awk '{print $7}'`
 is_removable=`lsblk | grep $1 | awk '{print $5}'`	# 0 means 'Not removable'
+detect_bootable_partition=`fdisk -l | grep $1 | awk '{print $6}'`
 lvm_encrypted_name="partition_encrypted"
 
 encrypt_disk () {
@@ -11,7 +12,9 @@ encrypt_disk () {
 
 		# Check whether or not the encryption of the partition is supported
 		if [ $mountpoint = "/" ] || [ $mountpoint = "[SWAP]" ]; then
-			echo "Sorry, $mountpoint encryption is not supported."
+			echo "Sorry, $mountpoint encryption is not supported." ; exit
+		elif [ $detect_bootable_partition = "EFI" ] || [ $detect_bootable_partition = "BIOS" ]; then
+			echo "The partition containing the bootloader must not be encrypted" ; exit
 		fi
 
 		# umount $mountpoint
